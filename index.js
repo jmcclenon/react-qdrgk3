783e8450590f841dd01d7035927d1d3e9a14dade
jmcclenon <mars.john2018@gmail.com>
2019-12-10 21:07:22 -0500

    1 file modified

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Hello from "./Hello";
+import "./style.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'React'
+      name: "React"
     };
   }
 
@@ -15,15 +15,11 @@ class App extends Component {
     return (
       <div>
         <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-         <p>
-          Watch as your code updates the screen automatically.
-        </p>
+        <p>Start editing to see some magic happen :)</p>
+        <p>Watch as your code updates the screen automatically.</p>
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
